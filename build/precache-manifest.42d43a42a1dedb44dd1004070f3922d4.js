be4e96ba3208ef5e80fffe5980664f34f11923a6
o <42962454+change-my-username@users.noreply.github.com>
2020-11-03 00:09:37 +0200

    Init

---
+++ build/precache-manifest.42d43a42a1dedb44dd1004070f3922d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080eb04ec6d430a24bb0614dfc2223e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc7d085f5491b4c27732",
+    "url": "/static/css/main.284b6de1.chunk.css"
+  },
+  {
+    "revision": "d424bc1d69b121f3deed",
+    "url": "/static/js/2.9772d05c.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.9772d05c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc7d085f5491b4c27732",
+    "url": "/static/js/main.dbdc447c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
